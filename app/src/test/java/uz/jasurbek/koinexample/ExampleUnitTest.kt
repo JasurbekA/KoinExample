1ce998f3bbc272deebfce02c671cb4109210f342
Jasurbek96 <jasurbek.abdiroziqov@gmail.com>
2020-09-10 15:49:44 +0500

    Check Modules Test added

--- app/src/test/java/uz/jasurbek/koinexample/ExampleUnitTest.kt
+++ app/src/test/java/uz/jasurbek/koinexample/ExampleUnitTest.kt
@@ -3,15 +3,30 @@ package uz.jasurbek.koinexample
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.experimental.categories.Category
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.category.CheckModuleTest
+import org.koin.test.check.checkModules
+import uz.jasurbek.koinexample.di.appModule
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+@Category(CheckModuleTest::class)
+class ExampleUnitTest : AutoCloseKoinTest() {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    /**
+     * Checking module if there is any conflict in Dependency graph
+     * */
+    @Test
+    fun checkKoinModules() : Unit = checkModules {
+        modules(appModule)
+    }
 }
+
